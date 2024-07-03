c7e2e8b190de452d2f2c03e793b6822306b98a95
Alex <scabbia@gmail.com>
2024-07-03 01:56:18 -0600

    asdf

--- app/Controllers/Sesion.php
+++ app/Controllers/Sesion.php
@@ -86,7 +86,7 @@ class Sesion extends BaseController
             $this->session->set( "usuario", $usuario->id );
 
             $db = db_connect();
-            $db->query( "select f_update_PTS( {$usuario->id}, '{$modelo}', '{$mescalifica}' )" );  
+            $db->query( "select f_update_PTS( {$usuario->id}, codigo, DATE_FORMAT( NOW(), '%Y%m') ) FROM t_modelos WHERE estatus_codigo = '201-ACTIVO'" );  
             $db->query( "select f_get_estatus( {$usuario->id} )" );
 
             // BITACORA inicio de sesión exitoso
