abfc98068d7d9caaea9a47193abcdd5633a76c48
Alex <scabbia@gmail.com>
2024-07-25 14:26:13 -0600

    asdf

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -29,11 +29,11 @@ class Admin extends BaseController
         $this->data[ "almacenes" ]    = model( "AlmacenModel" )->where( $sql , null, false )->findAll();
         $this->data[ "rangos" ]       = model( "RangoModel" )->findAll();
         $this->data[ "productos" ]    = model( "ProductoModel" )->where( $sql , null, false )->findAll();
-        $this->data[ "usuarios" ]     = model( "UsuarioModel" )->where( $sql , null, false )->findAll();
-     /*  $this->data[ "roles" ]        = model( "RolModel" )->findAll();
+     /*   $this->data[ "usuarios" ]     = model( "UsuarioModel" )->where( $sql , null, false )->findAll();*/
+       $this->data[ "roles" ]        = model( "RolModel" )->findAll();
         $this->data[ "periodos" ]     = model( "PeriodoModel" )->where( $sql , null, false )->findAll();
         $this->data[ "esquemas" ]     = model( "EsquemaModel" )->where( $sql , null, false )->findAll();
-         $this->data[ "recompensas" ]  = model( "RecompensaModel" )->where( $sql , null, false )->findAll(); */
+         $this->data[ "recompensas" ]  = model( "RecompensaModel" )->where( $sql , null, false )->findAll(); 
        
         echo "hola";
         die();
