005da65ea44c865e8b4b41d4cfcc59a4e467bbf4
Alex <scabbia@gmail.com>
2024-07-29 03:38:24 -0600

    asdf

--- app/Views/dashboard/bloques/14-MI-ESTATUS.php
+++ app/Views/dashboard/bloques/14-MI-ESTATUS.php
@@ -45,6 +45,8 @@ foreach( MODELOS as $m ){
         
             break;
         case "20-TELEFONIA": 
+
+            dd($m["codigo" ]);
             $cal .= "<td><div class=\"input-group input-xgroup-sm\" data-bs-toggle=\"tooltip\" title=\"\">
             <input data-bs-toggle=\"tooltip\" title=\"<span class='small'>{$t_0}</span><br>".(CALIFICACIONES[ $cx[ $m["codigo" ] ][ "m_0" ] ][ "descripcion" ])."\" disabled type=\"text\" value=\"".( intval( substr( $cx[ $m["codigo" ] ][ "m_0" ], 0, 2 ) ) >= 10 ?  $cx[ $m["codigo" ] ][ "m_0" ] : "" )."\" class=\"form-control py-2 text-center text-{$estatus[ "color" ]}\" style=\"font-weight:700; background:var(--bs-".( intval( substr( $cx[ $m["codigo" ] ][ "m_0" ], 0, 2 ) ) >= 10 ? "gray-300" : "gray-100" )."); border:none\"></div></td>";
         
