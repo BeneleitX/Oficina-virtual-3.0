e6eba8f1f6e398ce640ff3b6284dcb0e79ece0d6
Alex <scabbia@gmail.com>
2024-08-02 03:36:44 -0600

    layout bancos

--- app/Controllers/Bancos.php
+++ app/Controllers/Bancos.php
@@ -17,23 +17,6 @@ class Bancos extends BaseController
 
 
     public function analiza_layout(){
-/*         $spreadsheet = new Spreadsheet();
-        $sheet = $spreadsheet->getActiveSheet();
-        $sheet->setCellValue('A1', 'Hello World !');
-        $writer = new Xlsx($spreadsheet);
-
-        $modelo = "10-NUTRICION";
-        $path = "assets/archivo/corte/".$modelo;
-
-        if( !is_dir( $path ) ){
-            mkdir( $path );
-        }
-
-        $file = $path."/hello world.xlsx";
-
-        $writer->save("./".$file);        
-
-        echo base_url().$file; */
 
         $archivo   = $_FILES[ "archivo" ][ "name" ];
         $partes = explode( ".", $archivo );
@@ -52,18 +35,59 @@ class Bancos extends BaseController
         ];
         
         move_uploaded_file( $_FILES[ "archivo" ][ "tmp_name" ], $path.time()."_".$archivo );        
+        $referencias = [];
 
         // EXCEL
         if( in_array( $extension, [ "xls", "xlsx" ] ) ){
             $respuesta[ "logo_banco" ] = base_url()."assets/img/bancos/127.png";
             $respuesta[ "banco" ] = "AZTECA";
+
+            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load( $path.time()."_".$archivo );
+            $worksheet = $spreadsheet->getActiveSheet();
+            $excel = [];
+            foreach ($worksheet->getRowIterator() as $row) {
+                $fila = [];
+                $cellIterator = $row->getCellIterator();
+                $cellIterator->setIterateOnlyExistingCells(FALSE); 
+
+                foreach ($cellIterator as $cell) {
+                    $fila[] = trim( $cell->getValue() );
+                }
+
+                if( $fila[ 0 ] == "01720107476782" ){
+                    $excel[] = $fila;
+                }
+            }
+
+
+            foreach( $excel as $line ){
+                $respuesta[ "conteo" ][ "lineas" ]++;
+                if( strlen( $line[ 3 ] ) < 9 && strlen( $line[ 3 ] ) > 1 ){
+                    $respuesta[ "conteo" ][ "pagos" ]++;
+                    $valido = 1;
+
+                    $l = [
+                        "original" => $line,
+                        "fecha" => date( "Y-m-d", strtotime( $line[ 1 ] ) ),
+                        "pedido" => substr( $line[ 3 ], 0, -1 ),
+                        "referencia" => $line[ 3 ],
+                        "folio" => $line[ 6 ],
+                        "cantidad" => filter_var( $line[ 4 ], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION ),
+                        "socio" => null,
+                        "accion" => null,
+                    ];
+
+                    $respuesta[ "pagos" ][ $l[ "referencia" ] ] = $l;
+                    $referencias[] = $l[ "pedido" ];
+                }
+            }
+
         }
 
         // TXT
         elseif( $extension == "txt" ){
             $respuesta[ "logo_banco" ] = base_url()."assets/img/bancos/012.png";
             $respuesta[ "banco" ] = "BBVA";
-            $referencias = [];
 
             foreach( file( $path.time()."_".$archivo ) as $line ){
                 $respuesta[ "conteo" ][ "lineas" ]++;
@@ -118,7 +142,7 @@ class Bancos extends BaseController
 
                     if( $valido ){
                         $respuesta[ "pagos" ][ $l[ "referencia" ] ] = $l;
-                        $referencias[] = substr( $l[ "pedido" ], 0, -1 );
+                        $referencias[] = $l[ "pedido" ];
                     }
                     else{
                         echo json_encode( $respuesta );
@@ -126,19 +150,37 @@ class Bancos extends BaseController
                     }
                 }
             }
+        }
 
-            $pedidos = model( "PedidoModel" )->find( $referencias );
+        $pedidos = model( "PedidoModel" )->find( $referencias );
             
-            if( $pedidos ){
+        if( $pedidos ){
+            
+            foreach( $pedidos as $p ){
+
+                $u = model( "UsuarioModel" )->find( $p[ "usuario_id" ] );
+
+                if( substr( $p[ "estatus_codigo" ], 0, 3 ) < 300 ){
+
+                    $p[ "estatus_codigo" ] = "420-PAGADO";
+                    $p[ "fechas" ][ "pagado" ]   = $respuesta[ "pagos" ][ $p[ "referencia" ] ][ "fecha" ];     
+                    $p[ "fechas" ][ "califica" ] = $respuesta[ "pagos" ][ $p[ "referencia" ] ][ "fecha" ];    
+
+                    model( "PedidoModel" )->save( $p );
+
+                    $db = db_connect();
+                    $db->query( "select f_update_PTS( {$u->id}, '{$p[ "modelo_codigo" ]}', '".date( "Ym", strtotime( $respuesta[ "pagos" ][ $p[ "referencia" ] ][ "fecha" ] ) )."' )" );  
+                    $db->query( "select f_get_estatus( {$u->id} )" );
+                    $afectados = $db->query( "select f_reparte_comisiones( {$p[ "id" ]}, 0 )" )->getRow();
                 
-                foreach( $pedidos as $p ){
-                    $u = model( "UsuarioModel" )->find( $p[ "usuario_id" ] );
-                    $respuesta[ "pagos" ][ $p[ "referencia" ] ][ "socio" ] = $u->id()." ".$u->nombre( 2 );
-
-                    if( $p[ "estatus_codigo "] )
-
                     $respuesta[ "conteo" ][ "pagados" ]++;
+                    $respuesta[ "pagos" ][ $p[ "referencia" ] ][ "accion" ] = "<span class=\"badge bg-teal\">pagado</span>";
                 }
+                else{
+                    $respuesta[ "pagos" ][ $p[ "referencia" ] ][ "accion" ] = "<span class=\"badge bg-red\">ya procesado</span>";
+                }
+
+                $respuesta[ "pagos" ][ $p[ "referencia" ] ][ "socio" ] = $u->avatar( 24 )." ".$u->id( $p[ "modelo_codigo" ] )." ".$u->nombre( 2 );
             }
         }
 
