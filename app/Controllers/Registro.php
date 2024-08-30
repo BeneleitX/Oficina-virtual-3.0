74d55ce5fe51f2ba6879b4c7d65614214b09fee2
Alex <scabbia@gmail.com>
2024-08-30 12:23:38 -0600

    asdf

--- app/Controllers/Registro.php
+++ app/Controllers/Registro.php
@@ -247,6 +247,8 @@ class Registro extends BaseController
             return json_encode( $respuesta );
         }
 
+        // temporal.. hay que usar el estatus directo de la tabla
+        // pero ese se debe actualizar con la función f_get_estatus que está pendiente de revisar
         if( 
             substr( $patrocinador->data->estatus->modelos->{"10-NUTRICION"}, 0, 3 ) < 200 &&
             substr( $patrocinador->data->estatus->modelos->{"20-TELEFONIA"}, 0, 3 ) < 200 &&
