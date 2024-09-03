7241d1a0dc08b58491f9fa449ca9e370330a8a7d
Alex <scabbia@gmail.com>
2024-09-03 09:36:31 -0600

    asdf

--- app/Views/pedidos/beneleit_movil.php
+++ app/Views/pedidos/beneleit_movil.php
@@ -1,6 +1,4 @@
 
-
-<?php /*
 <script src="https://core.beneleit.talentonet.com/static/beneleit/beneleit.js"></script>
 <iframe id="iframe_beneleit" src="about:blank"></iframe>
 
@@ -16,11 +14,5 @@
 
     });    
 </script>
-*/ ?>
 
-<div class="row">
-    <div class="col-md-6 col-lg-4 offset-md-3 offset-lg-4">
-        <img src="<?php echo base_url(); ?>assets/img/logo_movil.png" class="img-fluid rounded">
-        <p class="display-4 my-5 text-center">Próximamente</p>
-    </div>
-</div>
+
