e897f5c073cd31b09cd1ee66c8829f6a4cfdc318
Alex <scabbia@gmail.com>
2024-09-12 15:40:53 -0600

    sadf

---
+++ app/Views/pedidos/gateways/23-GETNET.php
@@ -0,0 +1,32 @@
+<?php 
+helper( "getnet_helper" );
+
+$xml  = getCadenaXML( $pedido, $socio );
+$xurl = getCadenaURL( $xml );
+?>
+
+<p class="text-center"><a href="<?php echo base_url( "pedido/{$pedido[ "referencia" ]}" ); ?>" class="btn btn-danger"><i class="fa fa-undo"></i> Regresar al pedido</a></p>
+
+<iframe 
+    src="<?php echo $xurl; ?>"
+    width="100%" 
+    height="750px" 
+    frameborder="0" 
+    scrolling="no"
+    seamless="seamless"
+    id="getnet"
+  ></iframe> 
+  
+          
+<script>
+
+
+
+$(document).ready(function()
+{ 
+});
+
+
+
+
+</script>
