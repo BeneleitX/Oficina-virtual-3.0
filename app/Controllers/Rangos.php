c7721bd7d524a39a1b371a3320d419e424ee188b
Alex <scabbia@gmail.com>
2024-09-26 12:49:49 -0600

    dsaf

--- app/Controllers/Rangos.php
+++ app/Controllers/Rangos.php
@@ -23,7 +23,7 @@ class Rangos extends BaseController
         $this->data[ "modelo" ] = $modelo;
 
         $sql = "modelo_codigo = '{$modelo}' and SUBSTRING( codigo, 2,3 ) > 0";
-        $this->data[ "rangos" ] = model( "rangoModel" )->where( $sql , null, false )->findAll();
+        $this->data[ "rangos" ] = model( "RangoModel" )->where( $sql , null, false )->findAll();
 
         $db = db_connect();
         $socios = $db->query( "SELECT if( SUBSTRING( u.estatus_codigo, 1,3 ) > 200, \"activos\", \"inactivos\") AS estatus, u.data->>\"$.rango\" AS rango_codigo, COUNT(*) AS cantidad FROM t_usuarios u GROUP BY if( SUBSTRING( u.estatus_codigo, 1,3 ) > 200, \"ACTIVO\", \"INACTIVO\"), u.data->>\"$.rango\"" );
