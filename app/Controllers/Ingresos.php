a656203b0a893cbfdc3df64fea91b4e50fa2c91a
Alex <scabbia@gmail.com>
2024-10-17 21:14:45 -0600

    sadf

--- app/Controllers/Ingresos.php
+++ app/Controllers/Ingresos.php
@@ -68,7 +68,7 @@ class Ingresos extends BaseController
         load_catalogo( "esquemas", "modelo_codigo = '{$pago[ "modelo_codigo" ]}'");
 
         $sql  = "SELECT 
-                    c.fecha, 
+                    min(c.fecha), 
                     e.codigo as esquema, 
                     IFNULL( p.data->'$.factor', 2.5 ) as factor, 
                     SUM( c.cantidad ) as cantidad,
