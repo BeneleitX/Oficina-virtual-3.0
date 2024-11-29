5f9ef1c49d31369b6ce051e006756eef1104edd5
Alex <scabbia@gmail.com>
2024-11-29 16:23:40 -0600

    Plan de pagos

---
+++ app/Views/dashboard/bloques/21-LINKS.php
@@ -0,0 +1,45 @@
+<div class="card-body mb-4">
+<a href="https://www.beneleit.mx/plan-de-pagos-sinergy-de-beneleit/" target="_blank">
+    <table><tr>
+        <td><div class="card p-3 me-3"><img src="https://v4.app/assets/img/logo_color.png" width="100"></div></td>
+        <td width="100%">
+            <p class="m-0">Click aquí para consultar nuestro</p>
+            <h4 class="m-0">Plan de pagos</h4>
+        </td>
+    </tr></table>
+</a>
+</div>
+
+<h4 class="text-center">Información en video:</h4>
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+      <strong class="me-3"><i class="fab fa-youtube text-red" style="font-size:30px"></i></strong>SINERGY Beneleit nutrición parte 1 (detalles de comisiones, corte y pagos).
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body"><iframe title="1.- Beneleit Nutrición Sinergy (Part 1)" width="400" height="225" src="https://www.youtube.com/embed/mvRNn4fofew?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+      <strong class="me-3"><i class="fab fa-youtube text-red" style="font-size:30px"></i></strong>SINERGY Beneleit nutrición Parte 2 (Plan de reconocimientos, sistema de premios BeneStar, calificación y rangos)
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body"><iframe title="2.- Beneleit Nutrición Sinergy (Part 2)" width="400" height="225" src="https://www.youtube.com/embed/ifj9ypLcEXs?list=PLzR1E7MVK5Wyt9DedQQevRXBQVucpwo2F" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      <strong class="me-3"><i class="fab fa-youtube text-red" style="font-size:30px"></i></strong>Bonos Beneleit nutrición (Productos de regalo: productos a precio promoción, bono de productividad,  bono de lealtad. Bono de aniversario y bono por productos al 50%).
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body"><iframe title="3.- ¿Cómo funcionan los bonos Beneleit?" width="400" height="225" src="https://www.youtube.com/embed/TIaUYG1rpsk?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>
+    </div>
+  </div>
+</div>
