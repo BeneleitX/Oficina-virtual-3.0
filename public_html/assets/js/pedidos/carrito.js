b3bc2f51c14a05470201b0b5cacf615cd28b098e
Alex <scabbia@gmail.com>
2024-12-03 17:16:49 -0600

    asdf

--- public_html/assets/js/pedidos/carrito.js
+++ public_html/assets/js/pedidos/carrito.js
@@ -391,10 +391,13 @@ function update_pedido( flag = null ){
         cantidad.html( caption );
         costo_extra.html( 'Comisión bancaria por ' + Moneda.format( comision ) );
 
+
         es_paqueteria = pedido.metodoentrega_codigo ? pedido.metodoentrega_codigo.substring( 0, 2 ) != '00' && pedido.metodoentrega_codigo.substring( 0, 2 ) != '11' : false;
         
-            permitepagos = !pedido.no_stock && total_productos_pedido > 0 /* && ( total_productos_pedido > 0 || ( subtotal > 0  || total_saldo > 0) ) */ && parseInt( pedido.data.entrega ) > 0 && ( ( es_paqueteria && pedido.data.domicilio !== undefined || !es_paqueteria && pedido.data.entrega.length > 0 ) );
+        permitepagos = !pedido.no_stock && total_productos_pedido > 0 /* && ( total_productos_pedido > 0 || ( subtotal > 0  || total_saldo > 0) ) */ && parseInt( pedido.data.entrega ) > 0 && ( ( es_paqueteria && pedido.data.domicilio !== undefined || !es_paqueteria && pedido.data.entrega.length > 0 ) );
 
+        console.log( permitepagos, pendientes, pedido.no_stock, total_productos_pedido, pedido.data.entrega, es_paqueteria);
+        
         if( !permitepagos || pendientes ){
             $( this ).prop( 'disabled', true );
             $( this ).removeClass( 'btn-primary' );
