47d506cdcacad9bde1aaa7627ecf56ab2f425a0e
Alex <scabbia@gmail.com>
2025-03-12 23:52:40 -0600

    asdf

--- app/Views/dashboard/bloques/50-INVERSIONES.php
+++ app/Views/dashboard/bloques/50-INVERSIONES.php
@@ -107,16 +107,3 @@
     </table>
 </div>
 
-
-<?php
-
-if( $usuario->id == 55 ){
-    $inversiones = model( "InversionModel" )->findAll();
-
-    foreach( $inversiones as $i ){
-        $pedido = model( "PedidoModel" )->find( $i[ "pedido_id" ] );
-        $i[ "extras" ][ "meses" ] = genera_meses( $pedido );
-
-        model( "InversionModel" )->save( $i );
-    }
-}
