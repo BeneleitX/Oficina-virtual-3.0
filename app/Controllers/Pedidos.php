8c4a5d5c29e1fefb5a21358e3f2b81e3718391df
Alex <scabbia@gmail.com>
2025-03-14 14:34:31 -0600

    sdfg

--- app/Controllers/Pedidos.php
+++ app/Controllers/Pedidos.php
@@ -867,10 +867,10 @@ class Pedidos extends BaseController
         $fecha = date( "Y-m-d H:i:s" );
         $saldo = $u->saldo( $pedido[ "modelo_codigo" ] );
         
-        if( $hash == "saldo" || $hash == "unknown" ){
+        if( $hash == "saldo" || ( session( "admin" ) && $hash == "Capital24_hash" ) ){
             $pagado   = true;
             $hash    .= "_".time();
-            $cantidad = $hash == "unknown" ? $pedido[ "data" ][ "total" ] : $saldo;
+            $cantidad = $hash == "Capital24_hash" ? $pedido[ "data" ][ "total" ] : $saldo;
             $total    = $pedido[ "data" ][ "total" ];
             $tx       = [
                 "from_address" => "",
