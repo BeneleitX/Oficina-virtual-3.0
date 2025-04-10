970453e94ddbca7a53ab7c5f039352e36ccf4cda
Alex <scabbia@gmail.com>
2025-04-09 21:11:52 -0600

    fas

--- app/Controllers/Periodos.php
+++ app/Controllers/Periodos.php
@@ -397,7 +397,6 @@ class Periodos extends BaseController
                     p.usuario_id, 
                     p.clabe, 
                     p.data as p_data, 
-                    p.data->>'$.periodos.creacion' as periodo,
                     u.data as u_data, 
                     f_es_verificado( u.id) AS verificado, 
                     b.nombre as banco
@@ -448,7 +447,7 @@ class Periodos extends BaseController
                     //$pago[ "u_data" ][ "sat" ][ "rfc" ] ?? "",
                     strval( $pago[ "clabe" ] ),
                     30,
-                    "PAGO SEMANA ".periodo( $pago[ "periodo" ] ),
+                    "PAGO SEMANA ".periodo( $pago[ "p_data" ][ "periodos" ][ "creacion" ] ),
                     $promo = $importe * .1, // promo
                     $neto, // neto
                     $importe, // importe
@@ -467,7 +466,7 @@ class Periodos extends BaseController
                     //$pago[ "u_data" ][ "sat" ][ "rfc" ] ?? "",
                     strval( $pago[ "clabe" ] ),
                     30,
-                    "PAGO SEMANA ".periodo( $pago[ "periodo" ] ),
+                    "PAGO SEMANA ".periodo( $pago[ "p_data" ][ "periodos" ][ "creacion" ] ),
                     $subt = $pago[ "p_data" ][ "cantidades" ][ "subtotal" ] / 1.16, // subtotal
                     $pago[ "p_data" ][ "cantidades" ][ "subtotal" ], // importe
                     $rete = $subt * 0.1066, // retencion
@@ -484,7 +483,7 @@ class Periodos extends BaseController
                     $pago[ "u_data" ][ "nombre" ]." ".implode( " ", $pago[ "u_data" ][ "apellidos" ] ),
                     strval( $pago[ "clabe" ] ),
                     30,
-                    "PAGO SEMANA ".periodo( $pago[ "periodo" ] ),
+                    "PAGO SEMANA ".periodo( $pago[ "p_data" ][ "periodos" ][ "creacion" ] ),
                     $pago[ "banco" ],
                     $pago[ "p_data" ][ "cantidades" ][ "subtotal" ], 
                     $pago[ "p_data" ][ "cantidades" ][ "isr" ],
