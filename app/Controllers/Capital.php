7e113d811323feaf9dbb2cd95e3562b5c1983841
Alex <scabbia@gmail.com>
2025-06-01 22:28:20 -0600

    sadf

--- app/Controllers/Capital.php
+++ app/Controllers/Capital.php
@@ -1201,7 +1201,7 @@ class Capital extends BaseController
             $worksheet->setCellValue( chr(65 + $col++).$row, $u->id );
             $worksheet->setCellValue( chr(65 + $col++).$row, $u->nombre( 2, false, true ) );
             $worksheet->setCellValue( chr(65 + $col++).$row, $u->telefono );
-            $worksheet->setCellValue( chr(65 + $col++).$row, $u->data->wallet );
+            $worksheet->setCellValue( chr(65 + $col++).$row, $u->data->wallet ?? "NO WALLET" );
             $worksheet->setCellValue( chr(65 + $col++).$row, $cantidad );
         }
 
