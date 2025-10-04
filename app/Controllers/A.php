d1593db15642604ff70705d075c9570e2480b820
Alex <scabbia@gmail.com>
2025-10-03 22:34:01 -0600

    dfg

--- app/Controllers/A.php
+++ app/Controllers/A.php
@@ -6,7 +6,7 @@ class A extends BaseController
 
     public function Landing( $u ){
 
-        $request = base64_decode( urldecode( $u ) );
+        dd( $request = base64_decode( urldecode( $u ) ) );
         $socio = model( "UsuarioModel" )->where( "password = '{$request}'" )->first();
         echo "<p>Landing de ".$socio->nombre(2)."</p>";
     }
