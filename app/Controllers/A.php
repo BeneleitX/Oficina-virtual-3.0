e8d2d98e9861f8eebc5858cad3c75dc6bf90e715
Alex <scabbia@gmail.com>
2025-10-03 22:32:06 -0600

    sadf

--- app/Controllers/A.php
+++ app/Controllers/A.php
@@ -4,7 +4,10 @@
 class A extends BaseController
 {
 
-    public function Landing(){
-        echo "<p>Landing</p>";
+    public function Landing( $u ){
+
+        $request = base64_decode( urldecode( $u ) );
+        $socio = model( "UsuarioModel" )->where( "password = '{$request}'" )->first();
+        echo "<p>Landing de ".$socio->nombre(2)."</p>";
     }
 }
