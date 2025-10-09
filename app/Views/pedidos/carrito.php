9b78af81d50eab50d5a5189b4b93f076a002ed3e
Alex <scabbia@gmail.com>
2025-10-09 14:13:08 -0600

    boton rosa envio express

--- app/Views/pedidos/carrito.php
+++ app/Views/pedidos/carrito.php
@@ -252,7 +252,7 @@
                         // envio express gratis con 9 puntos o mas
 
                         elseif( ( $me[ "codigo" ] == "12-EXPRESS" ) && in_Array( $me[ "settings" ][ "tipocosto" ], $metodos ) ){
-                            echo "\n<input type=\"radio\" class=\"".( ( $pagado || $bloqueado || $cancelado ) && $me[ "codigo" ] != $pedido[ "metodoentrega_codigo" ] ? "d-none" : "" )." btn-check\" id=\"me-{$me[ "codigo" ]}\" autocomplete=\"off\" name=\"metodosentrega\" value=\"{$me[ "codigo" ]}\" ".( $me[ "codigo" ] == $pedido[ "metodoentrega_codigo" ] ? "checked" : "")." ><label class=\"d-none btn btn-outline-primary col-12 mb-1\" readonly for=\"me-{$me[ "codigo" ]}\">{$me[ "nombre" ]}</label>";
+                            echo "\n<input type=\"radio\" class=\"".( ( $pagado || $bloqueado || $cancelado ) && $me[ "codigo" ] != $pedido[ "metodoentrega_codigo" ] ? "d-none" : "" )." btn-check\" id=\"me-{$me[ "codigo" ]}\" autocomplete=\"off\" name=\"metodosentrega\" value=\"{$me[ "codigo" ]}\" ".( $me[ "codigo" ] == $pedido[ "metodoentrega_codigo" ] ? "checked" : "")." ><label class=\"".( ( $pagado || $bloqueado || $cancelado ) && $me[ "codigo" ] != $pedido[ "metodoentrega_codigo" ] ? "d-none" : "" )." btn btn-outline-pink col-12 mb-1\" readonly for=\"me-{$me[ "codigo" ]}\">{$me[ "nombre" ]}</label>";
                         }
                     }
                     /* }
