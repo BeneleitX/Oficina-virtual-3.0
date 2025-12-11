f65692030f111a62a3572bc2233888e5bc0d964a
Alex <scabbia@gmail.com>
2025-12-11 15:32:45 -0600

    asdf

--- app/Controllers/Sesion.php
+++ app/Controllers/Sesion.php
@@ -579,7 +579,8 @@ class Sesion extends BaseController
                     where usuario_id = {$usuario->id}
                     and substring( estatus_codigo,1 , 3 ) > 200
                     and fechas->>'$.mes' = '{$retiro[ "fechas" ][ "mes" ]}'
-                    and id != {$retiro[ "id" ]}
+                    and inversion_id = {$retiro[ "inversion_id" ]}
+                    // and id != {$retiro[ "id" ]}
                 " )->getRow()->total;
 
                 if( $existe ){
