8672d0514dd8e7ae64d446d05cd359b894c77274
Alex <scabbia@gmail.com>
2026-01-04 21:38:35 -0600

    sadf

--- app/Controllers/Capital.php
+++ app/Controllers/Capital.php
@@ -1455,15 +1455,18 @@ class Capital extends BaseController
 
         // validar cantidad
 
-        if( $cant != $bolsa ){
-            $this->data[ "usuario" ]->revisa_bono_liderazgo( $ps, $fecha );
+        if( $cant != $bolsa && $mes == date( "Ym" ) ){
 
+            $h = $this->data[ "usuario" ]->historial;
 
-/*             $h = $this->data[ "usuario" ]->historial;
+            if( !isset( $h->modelos->{"50-INVERSION"}->corte_mensual->{$mes} ) ){
+                $this->data[ "usuario" ]->revisa_bono_liderazgo( $ps, $fecha );
+            }
+         
             $h->modelos->{"50-INVERSION"}->corte_mensual->{$mes}->bolsa = $bolsa;
             $this->data[ "usuario" ]->historial = $h;
 
-            model( "UsuarioModel" )->save( $this->data[ "usuario" ] ); */
+            model( "UsuarioModel" )->save( $this->data[ "usuario" ] ); 
 
             // BITACORA Ajuste automatico bolsa capital semilla de red
             bitacora( 95, $this->data[ "usuario" ]->id, [ 
