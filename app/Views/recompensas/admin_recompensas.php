0eed247457a3d15a150a586f74f9e4a3f463a8ef
Alex <scabbia@gmail.com>
2026-02-03 13:42:29 -0600

    sadf

--- app/Views/recompensas/admin_recompensas.php
+++ app/Views/recompensas/admin_recompensas.php
@@ -1,15 +1,4 @@
-<?php 
 
-$usuario = model( "UsuarioModel" )->find( 166980 );
-
-$u1 = array_sum( $usuario->getNuevosSocios( 6, "2025-10-01", "2025-10-31" ) );
-$u2 = array_sum( $usuario->getNuevosSocios( 6, "2025-11-01", "2025-11-30" ) );
-$u3 = array_sum( $usuario->getNuevosSocios( 6, "2025-12-01", "2025-12-31" ) );
-$u4 = array_sum( $usuario->getNuevosSocios( 6, "2026-01-01", "2026-01-31" ) );
-
-echo $usuario->id." - ".$u1." - ".$u2." - ".$u3." - ".$u4." - ".($u1 + $u2 + $u3 + $u4 );
-
-?>
 <link href="<?php echo base_url(); ?>assets/css/datatables.css" rel="stylesheet"/>
 <script src="<?php echo base_url(); ?>assets/js/datatables.js" type="text/javascript"></script>
 <script src="<?php echo base_url(); ?>assets/js/datatables_bs5.js" type="text/javascript"></script>
