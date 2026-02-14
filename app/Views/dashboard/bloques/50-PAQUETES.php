f95bbe6202a1f0abff1d1779a847be0a52f747d7
Alex <scabbia@gmail.com>
2026-02-14 17:24:16 -0600

    sadf

--- app/Views/dashboard/bloques/50-PAQUETES.php
+++ app/Views/dashboard/bloques/50-PAQUETES.php
@@ -106,6 +106,7 @@ if( $saldo > 0 ){
 
             $k = $inv[ $p->codigo ];
 
+            if( $k[ "total" ] )
             echo "\n<a href=\"".base_url()."paquete\" class=\"btn w-100 border border-{$p->data->color} p-0 mb-1\"><div class=\"row m-0\">
                     <div class=\"col-7 py-2 text-start\">
                         <h5 class=\"m-0 text-".( $k[ "total" ] ? $p->data->color : "gray-400" )."\">{$p->data->nombre}".( $k[ "total" ] ? " <span class=\"badge bg-{$p->data->color}\">{$k[ "inversiones" ]}</span>" : "")."</h5>
